1bde1466900762ad131249e6d379ebb298f1779c
RomanPopov <roman2018.popov@yandex.ru>
2023-03-12 21:47:41 +0700

    Выполнил 4 лабу

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,11 +1,11 @@
-#include <string>
+﻿#include <string>
 #include <QCoreApplication>
 #include <QTextStream>
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#define N 15
+#define N 7
 
 using namespace std;
 
@@ -40,10 +40,40 @@ public:
         return weight;
     }
 
+    void GetPositionInfo() {
+        for( int i = 0; i < N; i++) {
+            cout<<"#"<<i+1<<" "<< points[i].GetInfo()<<endl;
+        }
+    }
+
+    class Point {
+    private:
+        int x;
+        int y;
+
+    public:
+        Point(int x, int y) {
+            this -> x = x;
+            this -> y = y;
+        }
+        string GetInfo() {
+            return "Point: x = " + to_string(x) + " " + "y = " + to_string(y);
+        }
+    };
+    Point points[N] {
+        Point(0, 0),
+        Point(3, 6),
+        Point(2, 4),
+        Point(2, 9),
+        Point(4, 11),
+        Point(8, 6),
+        Point(9, 9)
+    };
+
 };
 
 int main() {
     Human Pavel;
-
+    Pavel.GetPositionInfo();
     return 0;
 }
